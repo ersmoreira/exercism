ba94dbd8a635953eff3f29d85ca40caef098d33f
Edgar Moreira <ersmoreira@gmail.com>
2022-02-15 18:02:11 +0000

    Solve raindrops exercise

--- java/raindrops/src/main/java/RaindropConverter.java
+++ java/raindrops/src/main/java/RaindropConverter.java
@@ -1,7 +1,23 @@
 class RaindropConverter {
 
     String convert(int number) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        String aux = "";
+
+        if (number % 3 == 0) {
+            aux += "Pling";
+        }
+        if (number % 5 == 0) {
+            aux += "Plang";
+        }
+        if (number % 7 == 0) {
+            aux += "Plong";
+        }
+
+        if (aux.isEmpty()){
+            aux = Integer.toString(number);
+        }
+
+        return aux;
     }
 
 }
